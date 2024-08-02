680e478b0e051692bd62b75a59b5fe8a2956ddf0
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-08-03 00:26:09 +0530

    add s3 and

---
+++ cmd/utils/template_copy.go
@@ -0,0 +1,58 @@
+package utils
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+// Initialize S3 client
+var s3Client *s3.S3
+
+func init() {
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region:      aws.String("ap-south-1"), // Update with your region
+		Credentials: credentials.NewStaticCredentials("AKIATEWT4JZLG5RVPFB7", "mWC2/ATt22pEbBgB/hD8f2qTB0/QDqNsky5JdxMs", ""),
+	}))
+	s3Client = s3.New(sess)
+}
+
+// CopyTemplate loads the template from S3 and copies it to the destination file
+func CopyTemplate(s3Bucket, s3Key, dest string, replacements ...map[string]string) error {
+	result, err := s3Client.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(s3Bucket),
+		Key:    aws.String(s3Key),
+	})
+	if err != nil {
+		fmt.Println("Error fetching from S3:", err)
+		return err
+	}
+	defer result.Body.Close()
+
+	s3Content, err := io.ReadAll(result.Body)
+	if err != nil {
+		fmt.Println("Error reading S3 content:", err)
+		return err
+	}
+
+	content := string(s3Content)
+
+	if len(replacements) > 0 {
+		for placeholder, replacement := range replacements[0] {
+			content = strings.ReplaceAll(content, placeholder, replacement)
+		}
+	}
+
+	err = os.WriteFile(dest, []byte(content), 0644)
+	if err != nil {
+		fmt.Println("Error writing to destination file:", err)
+		return err
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -2,10 +2,6 @@ module github.com/TheRSTech/Backendforger-backend
 
 go 1.22.4
 
-require github.com/fatih/color v1.17.0
+require github.com/aws/aws-sdk-go v1.55.5
 
-require (
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-)
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
